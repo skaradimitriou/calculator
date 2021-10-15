d8513ee84e3879c28fce0922e17a0b4a1535f907
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-10-16 00:00:36 +0300

    Calculator Screen | UI

--- app/src/main/java/com/stathis/calculator/features/calculator/CalculatorActivity.kt
+++ app/src/main/java/com/stathis/calculator/features/calculator/CalculatorActivity.kt
@@ -1,8 +1,11 @@
 package com.stathis.calculator.features.calculator
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import com.stathis.calculator.R
 import com.stathis.calculator.abstraction.SimplifiedActivity
+import com.stathis.calculator.features.currencies.CurrenciesActivity
+import kotlinx.android.synthetic.main.activity_calculator.*
 
 class CalculatorActivity : SimplifiedActivity(R.layout.activity_calculator) {
 
@@ -16,6 +19,12 @@ class CalculatorActivity : SimplifiedActivity(R.layout.activity_calculator) {
         /*
         FIXME: Create UI layout for Calculator screen according to zeplin screen
          */
+
+        currencies_btn.setOnClickListener {
+            startActivity(Intent(this,CurrenciesActivity::class.java).also {
+                it.putExtra("AMOUNT",13500)
+            })
+        }
     }
 
     override fun stopOps() {
