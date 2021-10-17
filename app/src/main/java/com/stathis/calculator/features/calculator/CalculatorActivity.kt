b24279e2935f20b2ce37577460ed45bc99746955
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-10-17 23:52:09 +0300

    Calculator Logic | Init

--- app/src/main/java/com/stathis/calculator/features/calculator/CalculatorActivity.kt
+++ app/src/main/java/com/stathis/calculator/features/calculator/CalculatorActivity.kt
@@ -1,6 +1,9 @@
 package com.stathis.calculator.features.calculator
 
 import android.content.Intent
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.lifecycle.ViewModelProvider
 import com.stathis.calculator.R
 import com.stathis.calculator.abstraction.SimplifiedActivity
@@ -9,26 +12,64 @@ import kotlinx.android.synthetic.main.activity_calculator.*
 
 class CalculatorActivity : SimplifiedActivity(R.layout.activity_calculator) {
 
-    private lateinit var viewModel : CalculatorViewModel
+    private lateinit var viewModel: CalculatorViewModel
+    private var operationEnabled = false
+    private var decimalEnabled = true
 
     override fun init() {
-       viewModel = ViewModelProvider(this).get(CalculatorViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(CalculatorViewModel::class.java)
     }
 
     override fun startOps() {
-        /*
-        FIXME: Create UI layout for Calculator screen according to zeplin screen
-         */
-
         currencies_btn.setOnClickListener {
             val amount = calculator_result.text.toString()
-            startActivity(Intent(this,CurrenciesActivity::class.java).also {
-                it.putExtra("AMOUNT",amount)
+            startActivity(Intent(this, CurrenciesActivity::class.java).also {
+                it.putExtra("AMOUNT", amount)
             })
         }
     }
 
-    override fun stopOps() {
-        //
+    override fun stopOps() {}
+
+    fun allClearAction(view: View) {
+        calculator_operations.text = resources.getString(R.string.empty)
+        calculator_result.text = resources.getString(R.string.empty)
+    }
+
+    fun numberAction(view: View) {
+        when (view) {
+            is Button -> {
+                if (view.text == ".") {
+                    if (decimalEnabled) {
+                        calculator_operations.append(view.text)
+                        decimalEnabled = false
+                    }
+                } else {
+                    calculator_operations.append(view.text)
+                }
+                operationEnabled = true
+            }
+        }
+    }
+
+    fun operationAction(view: View) {
+        when (view is Button && operationEnabled) {
+            true -> {
+                calculator_operations.append(view.text)
+                operationEnabled = false
+                decimalEnabled = true
+            }
+        }
+    }
+
+    fun equalsAction(view: View) {
+        calculator_result.text = calculator_operations.text
+    }
+
+    fun backspaceAction(view: View) {
+        when (calculator_operations.text.length) {
+            0 -> Unit
+            else -> calculator_operations.text = calculator_operations.text.dropLast(1)
+        }
     }
 }
--- app/src/main/java/com/stathis/calculator/features/currencies/CurrenciesActivity.kt
+++ app/src/main/java/com/stathis/calculator/features/currencies/CurrenciesActivity.kt
@@ -1,6 +1,9 @@
 package com.stathis.calculator.features.currencies
 
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.stathis.calculator.R
 import com.stathis.calculator.abstraction.SimplifiedActivity
@@ -25,9 +28,32 @@ class CurrenciesActivity : SimplifiedActivity(R.layout.activity_currencies) {
         Log.d("",amount.toString())
 
         start_currency_value.text = amount.toString()
+
+        val currencies = resources.getStringArray(R.array.currencies)
+        val arrayAdapter = ArrayAdapter(this,R.layout.dropdown_item,currencies)
+
+        start_cur_value.setAdapter(arrayAdapter)
+        end_cur_value.setAdapter(arrayAdapter)
+
+        convert_btn.setOnClickListener {
+            val currencyFrom = start_cur_value.text.toString()
+            val currencyTo = end_cur_value.text.toString()
+
+            viewModel.validateInput(currencyFrom,currencyTo)
+        }
+
+        viewModel.observe(this)
+
+        viewModel.wrongInput.observe(this, Observer{
+            when(it){
+                //FIXME: Save the final output text to resources
+                true -> Toast.makeText(this,"Please select the currencies that you want to convert", Toast.LENGTH_LONG).show()
+                false -> Unit
+            }
+        })
     }
 
     override fun stopOps() {
-        //
+        viewModel.release(this)
     }
 }
